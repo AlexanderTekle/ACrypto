840d4eb423470bc42c39456e8203a1f606009f6c
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2017-06-28 00:07:18 +0530

    complete all http functions

--- server/functions/index.js
+++ server/functions/index.js
@@ -19,15 +19,108 @@ app.use(cors);
 // Get all coins, optionally specifying a type to filter on
 app.get('/coins', (req, res) => {
   const type = req.query.type;
-  let ref = admin.database().ref(`/master/coins`);
+  var filter = 0;
+  if (type && ['arbitrage'].indexOf(type) > -1) {
+     filter = 1;
+  }
 
-  ref.orderByChild('order')
+  admin.database().ref(`/master/coins`)
+  .orderByChild("order")
   .once('value')
   .then(snapshot => {
     var value = snapshot.val();
     if (value) {
-        var list = snapshot.val();
-        return res.status(200).json({coins: list});
+        var messages = [];
+        snapshot.forEach(childSnapshot => {
+            if(filter == 1){
+              if(!childSnapshot.hasChild(type)){
+                return;
+              }
+            }
+            messages.push({code: childSnapshot.key, name: childSnapshot.val().name, order: childSnapshot.val().order});
+        });
+        return res.status(200).json({coins: messages});
+    } else {
+        res.status(401).json({error: 'No data found'});
+    }
+  }).catch(error => {
+    console.log('Error getting messages', error.message);
+    res.sendStatus(500);
+  });
+});
+
+// GET /api/currency?type={type}
+// Get all currency, optionally specifying a type to filter on
+app.get('/currency', (req, res) => {
+  const type = req.query.type;
+  var filter = 0;
+  if (type && ['arbitrage_from', 'arbitrage_to'].indexOf(type) > -1) {
+     filter = 1;
+  }
+
+  admin.database().ref(`/master/currency`)
+  .orderByChild("order")
+  .once('value')
+  .then(snapshot => {
+    var value = snapshot.val();
+    if (value) {
+        var messages = [];
+        snapshot.forEach(childSnapshot => {
+            if(filter == 1){
+              if(!childSnapshot.hasChild(type)){
+                return;
+              }
+            }
+            messages.push({code: childSnapshot.key, name: childSnapshot.val().name, order: childSnapshot.val().order});
+        });
+        return res.status(200).json({currency: messages});
+    } else {
+        res.status(401).json({error: 'No data found'});
+    }
+  }).catch(error => {
+    console.log('Error getting messages', error.message);
+    res.sendStatus(500);
+  });
+});
+
+// GET /api/coins_list
+// Get all coins list
+app.get('/coins_list', (req, res) => {
+
+  admin.database().ref(`/master/coins_list`)
+  .orderByChild("order")
+  .once('value')
+  .then(snapshot => {
+    var value = snapshot.val();
+    if (value) {
+      var messages = [];
+      snapshot.forEach(childSnapshot => {
+        messages.push({code: childSnapshot.key});
+      });
+      return res.status(200).json({coins_list: messages});
+    } else {
+        res.status(401).json({error: 'No data found'});
+    }
+  }).catch(error => {
+    console.log('Error getting messages', error.message);
+    res.sendStatus(500);
+  });
+});
+
+// GET /api/symbols
+// Get symbol of all currency codes
+app.get('/symbols', (req, res) => {
+
+  admin.database().ref(`/master/symbols`)
+  .once('value')
+  .then(snapshot => {
+    var value = snapshot.val();
+    if (value) {
+      var messages = [];
+      snapshot.forEach(childSnapshot => {
+        messages.push({code: childSnapshot.key, symbol: childSnapshot.val()});
+      });
+      return res.status(200).json({symbols: messages});
     } else {
         res.status(401).json({error: 'No data found'});
     }
